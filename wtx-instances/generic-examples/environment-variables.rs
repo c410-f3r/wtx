ad58fc3c209c732cc2f8e72d960b1183a1e0941c
Caio <c410.f3r@gmail.com>
2025-12-09 09:20:40 -0300

    Improve FromVars

--- wtx-instances/generic-examples/environment-variables.rs
+++ wtx-instances/generic-examples/environment-variables.rs
@@ -4,19 +4,34 @@ extern crate wtx;
 extern crate wtx_macros;
 
 use std::sync::OnceLock;
-use wtx::misc::EnvVars;
+use wtx::{
+  calendar::{DateTime, Utc},
+  misc::EnvVars,
+};
 
 static VARS: OnceLock<Vars> = OnceLock::new();
 
 fn main() -> wtx::Result<()> {
   let _rslt = VARS.set(EnvVars::from_available()?.finish());
-  let Vars { origin, rust_log } = VARS.wait();
-  println!("`ORIGIN={origin}` and `RUST_LOG={rust_log}`");
+  let Vars { now, origin, port, rust_log } = VARS.wait();
+  println!("`NOW={now:?}`, `ORIGIN={origin}`, `PORT={port}` and `RUST_LOG={rust_log:?}`");
   Ok(())
 }
 
 #[derive(Debug, wtx_macros::FromVars)]
 struct Vars {
+  #[from_vars(map_now)]
+  now: Option<DateTime<Utc>>,
   origin: String,
-  rust_log: String,
+  #[from_vars(map_port)]
+  port: u16,
+  rust_log: Option<String>,
+}
+
+fn map_now(var: String) -> wtx::Result<DateTime<Utc>> {
+  Ok(DateTime::from_iso_8601(var.as_bytes())?)
+}
+
+fn map_port(var: String) -> wtx::Result<u16> {
+  Ok(var.parse()?)
 }
--- wtx-macros-tests/tests/expand/from_vars.rs
+++ wtx-macros-tests/tests/expand/from_vars.rs
@@ -1,7 +1,19 @@
 #[derive(wtx_macros::FromVars)]
 pub struct Foo {
-    bar: String,
-    baz: String,
+  bar0: String,
+  bar1: Option<String>,
+  #[from_vars(parse_bar2)]
+  bar2: u16,
+  #[from_vars(parse_bar3)]
+  bar3: Option<u32>,
+}
+
+fn parse_bar2(value: String) -> wtx::Result<u16> {
+    Ok(value.parse()?)
+}
+
+fn parse_bar3(value: String) -> wtx::Result<u32> {
+    Ok(value.parse()?)
 }
 
 fn main() {}
