2baaad62cb3573b3321d5d2f2c36b81bd6763c9d
Caio <c410.f3r@gmail.com>
2026-01-30 09:17:24 -0300

    wtx-0.41.8

--- .scripts/internal-tests0.sh
+++ .scripts/internal-tests0.sh
@@ -3,7 +3,7 @@
 . "$(dirname "$0")/common.sh" --source-only
 
 $rt rustfmt
-$rt clippy -Aclippy::as_conversions,-Aclippy::modulo_arithmetic,-Aclippy::arbitrary_source_item_ordering,-Aclippy::doc-include-without-cfg,-Aclippy::little-endian-bytes,-Aclippy::panic-in-result-fn,-Aclippy::return_and_then,-Aclippy::used_underscore_items
+$rt clippy -Aclippy::as_conversions,-Aclippy::cfg_not_test,-Aclippy::modulo_arithmetic,-Aclippy::arbitrary_source_item_ordering,-Aclippy::doc-include-without-cfg,-Aclippy::little-endian-bytes,-Aclippy::panic-in-result-fn,-Aclippy::return_and_then,-Aclippy::used_underscore_items
 
 MIRIFLAGS="-Zmiri-disable-isolation" cargo miri test --features http2,postgres,web-socket -p wtx
 
--- wtx/src/rng/cha_cha20.rs
+++ wtx/src/rng/cha_cha20.rs
@@ -1,96 +1,100 @@
-#![allow(clippy::wrong_self_convention, reason = "`to_*` is allowed for self references")]
-
-// 1 iteration = 2 rounds = 8 quarter rounds
+// ****** A word is `u32` or 4 bytes *****
+//
+// `#[inline(always)]` made a huge positive difference in practically all places.
 
 use crate::rng::{CryptoRng, Rng, SeedableRng};
 use core::fmt::Debug;
 
+#[cfg(test)]
+const BLOCKS_LEN: usize = 1;
+#[cfg(not(test))]
+const BLOCKS_LEN: usize = _simd! {
+  4 => 1,
+  16 => 4,
+  32 => 8,
+  // At the time of this writing, 16 didn't increased performance.
+  // Probably not worth it. The stack would also be a lot larger.
+  64 => 8
+};
+// 1 iteration = 2 rounds = 8 quarter rounds
 const ITERATIONS: u8 = 10;
-// Each word has 32 bits or 4 bytes
-const WORDS: usize = 16;
+const TOTAL_WORDS: usize = BLOCKS_LEN * WORDS_PER_BLOCK;
+const WORDS_PER_BLOCK: usize = 16;
 
 /// `ChaCha` block function with 20 rounds and a nonce of 12 bytes as specified in
 /// <https://datatracker.ietf.org/doc/html/rfc7539>.
 ///
-/// This structure is `Copy` to allow usage with `AtomicCell` in concurrent scenarios. You should
-/// probably use other implementations if performance or auditability is a concern.
+/// This structure is `Copy` to allow usage with `AtomicCell` in concurrent scenarios.
 #[derive(Clone, Copy, Eq, Ord, PartialEq, PartialOrd)]
 pub struct ChaCha20 {
-  block: Block,
-  idx: u8,
-  output: [u32; WORDS],
+  block: ParBlock,
+  idx: u16,
+  output: ParBlock,
 }
 
 impl ChaCha20 {
-  /// Creates a new instance with a `[0; 12]` nonce.
-  #[inline]
-  pub const fn from_key(key: [u8; 32]) -> ChaCha20 {
-    ChaCha20 { block: Block::new(key, [0; 12]), idx: 16, output: [0; WORDS] }
+  /// New instance with a zeroed nonce
+  #[inline(always)]
+  pub fn from_key(key: [u8; 32]) -> ChaCha20 {
+    ChaCha20 {
+      block: ParBlock::from_key_and_nonce(key, [0; 12]),
+      // FIXME(STABLE): const { TOTAL_WORDS.into() }
+      idx: u16::try_from(TOTAL_WORDS).unwrap_or_default(),
+      output: ParBlock::new(),
+    }
   }
 
   /// Creates a new instance where you are responsible for providing parameters.
   ///
   /// Ideally, `key` should have a high entropy.
-  #[inline]
-  pub const fn new(key: [u8; 32], nonce: [u8; 12]) -> ChaCha20 {
-    ChaCha20 { block: Block::new(key, nonce), idx: 16, output: [0; WORDS] }
-  }
-
-  const fn increment_counter(&mut self) {
-    const fn fun(num: &mut u32) -> bool {
-      let (rslt, overflow) = num.overflowing_add(1);
-      *num = rslt;
-      !overflow
+  #[inline(always)]
+  pub fn new(key: [u8; 32], nonce: [u8; 12]) -> ChaCha20 {
+    ChaCha20 {
+      block: ParBlock::from_key_and_nonce(key, nonce),
+      // FIXME(STABLE): const { TOTAL_WORDS.into() }
+      idx: u16::try_from(TOTAL_WORDS).unwrap_or_default(),
+      output: ParBlock::new(),
     }
-    if fun(&mut self.block.block_counter) {
-      return;
-    }
-    if fun(self.block.first_nonce_mut()) {
-      return;
-    }
-    if fun(self.block.second_nonce_mut()) {
-      return;
-    }
-    let _ = fun(self.block.third_nonce_mut());
   }
 }
 
 impl CryptoRng for ChaCha20 {}
 
-impl Debug for ChaCha20 {
-  #[inline]
-  fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-    f.debug_struct("ChaCha20").finish()
-  }
-}
-
 impl Rng for ChaCha20 {
-  #[inline]
+  #[inline(always)]
   fn u8(&mut self) -> u8 {
     self.u8_4()[0]
   }
 
-  #[inline]
+  #[inline(always)]
   fn u8_4(&mut self) -> [u8; 4] {
-    if usize::from(self.idx) == WORDS {
-      let lock_block = block_function::<true>(&self.block);
+    if usize::from(self.idx) >= TOTAL_WORDS {
+      block_function::<true>(&self.block, &mut self.output);
       self.idx = 0;
-      self.output = lock_block.to_words();
-      self.increment_counter();
+      self.block.increment_counter();
     }
-    let rslt = self.output.get(usize::from(self.idx) % WORDS).copied().unwrap_or_default();
+    let idx = usize::from(self.idx);
+    let block_idx = idx % BLOCKS_LEN;
+    let word_idx = idx / BLOCKS_LEN;
+    let rslt = self
+      .output
+      .0
+      .get(word_idx)
+      .and_then(|elem| elem.0.get(block_idx))
+      .copied()
+      .unwrap_or_default();
     self.idx = self.idx.wrapping_add(1);
     rslt.to_le_bytes()
   }
 
-  #[inline]
+  #[inline(always)]
   fn u8_8(&mut self) -> [u8; 8] {
     let [a, b, c, d] = self.u8_4();
     let [e, f, g, h] = self.u8_4();
     [a, b, c, d, e, f, g, h]
   }
 
-  #[inline]
+  #[inline(always)]
   fn u8_16(&mut self) -> [u8; 16] {
     let [a, b, c, d] = self.u8_4();
     let [e, f, g, h] = self.u8_4();
@@ -99,7 +103,7 @@ impl Rng for ChaCha20 {
     [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p]
   }
 
-  #[inline]
+  #[inline(always)]
   fn u8_32(&mut self) -> [u8; 32] {
     let [b0, b1, b2, b3] = self.u8_4();
     let [b4, b5, b6, b7] = self.u8_4();
@@ -119,243 +123,241 @@ impl Rng for ChaCha20 {
 impl SeedableRng for ChaCha20 {
   type Seed = [u8; 32];
 
-  #[inline]
+  #[inline(always)]
   fn from_seed(seed: Self::Seed) -> crate::Result<Self> {
     Ok(Self::from_key(seed))
   }
 }
 
-#[cfg_attr(test, derive(Debug))]
-#[derive(Clone, Copy, Eq, Ord, PartialEq, PartialOrd)]
-struct Block {
-  constants: [u32; 4],
-  keys: [u32; 8],
-  block_counter: u32,
-  nonces: [u32; 3],
+impl Debug for ChaCha20 {
+  #[inline]
+  fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+    f.debug_struct("ChaCha20").finish()
+  }
 }
 
-impl Block {
-  const fn from_rows(a: Row, b: Row, c: Row, d: Row) -> Self {
-    let Row(a0, a1, a2, a3) = a;
-    let Row(b0, b1, b2, b3) = b;
-    let Row(c0, c1, c2, c3) = c;
-    let Row(d0, d1, d2, d3) = d;
-    Self {
-      constants: [a0, a1, a2, a3],
-      keys: [b0, b1, b2, b3, c0, c1, c2, c3],
-      block_counter: d0,
-      nonces: [d1, d2, d3],
-    }
+/// Parallel Block
+///
+/// For one block made up of single words, 4 * 16 bytes = 64 bytes = 512 bits
+#[cfg_attr(test, derive(Debug))]
+#[derive(Clone, Copy, Eq, Ord, PartialEq, PartialOrd)]
+struct ParBlock([ParWord; WORDS_PER_BLOCK]);
+
+impl ParBlock {
+  #[inline(always)]
+  fn new() -> Self {
+    Self([ParWord::default(); WORDS_PER_BLOCK])
   }
 
   #[cfg(test)]
-  const fn from_words(words: [u32; WORDS]) -> Self {
-    let [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p] = words;
-    Self {
-      constants: [a, b, c, d],
-      keys: [e, f, g, h, i, j, k, l],
-      block_counter: m,
-      nonces: [n, o, p],
+  #[inline(always)]
+  fn from_words(words: [u32; WORDS_PER_BLOCK]) -> Self {
+    let mut rslt = [ParWord::default(); WORDS_PER_BLOCK];
+    for (from, to) in words.iter().zip(&mut rslt) {
+      to.fill(*from);
     }
+    Self(rslt)
   }
 
-  const fn new(key: [u8; 32], nonce: [u8; 12]) -> Self {
+  #[inline(always)]
+  fn from_key_and_nonce(key: [u8; 32], nonce: [u8; 12]) -> Self {
     #[rustfmt::skip]
     let [
       k0, k1, k2, k3, k4, k5, k6, k7, k8, k9, k10, k11, k12, k13, k14, k15,
       k16, k17, k18, k19, k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k30, k31,
     ] = key;
     let [n0, n1, n2, n3, n4, n5, n6, n7, n8, n9, n10, n11] = nonce;
-    Self {
-      constants: [0x6170_7865, 0x3320_646E, 0x7962_2D32, 0x6B20_6574],
-      keys: [
-        u32::from_le_bytes([k0, k1, k2, k3]),
-        u32::from_le_bytes([k4, k5, k6, k7]),
-        u32::from_le_bytes([k8, k9, k10, k11]),
-        u32::from_le_bytes([k12, k13, k14, k15]),
-        u32::from_le_bytes([k16, k17, k18, k19]),
-        u32::from_le_bytes([k20, k21, k22, k23]),
-        u32::from_le_bytes([k24, k25, k26, k27]),
-        u32::from_le_bytes([k28, k29, k30, k31]),
-      ],
-      block_counter: 0,
-      nonces: [
-        u32::from_le_bytes([n0, n1, n2, n3]),
-        u32::from_le_bytes([n4, n5, n6, n7]),
-        u32::from_le_bytes([n8, n9, n10, n11]),
-      ],
-    }
+    Self([
+      ParWord::from_elem(0x6170_7865),
+      ParWord::from_elem(0x3320_646E),
+      ParWord::from_elem(0x7962_2D32),
+      ParWord::from_elem(0x6B20_6574),
+      ParWord::from_elem(u32::from_le_bytes([k0, k1, k2, k3])),
+      ParWord::from_elem(u32::from_le_bytes([k4, k5, k6, k7])),
+      ParWord::from_elem(u32::from_le_bytes([k8, k9, k10, k11])),
+      ParWord::from_elem(u32::from_le_bytes([k12, k13, k14, k15])),
+      ParWord::from_elem(u32::from_le_bytes([k16, k17, k18, k19])),
+      ParWord::from_elem(u32::from_le_bytes([k20, k21, k22, k23])),
+      ParWord::from_elem(u32::from_le_bytes([k24, k25, k26, k27])),
+      ParWord::from_elem(u32::from_le_bytes([k28, k29, k30, k31])),
+      ParWord::from_array({
+        let mut idx: u32 = 0;
+        core::array::from_fn(|_| {
+          let rslt = idx;
+          idx = idx.wrapping_add(1);
+          rslt
+        })
+      }),
+      ParWord::from_elem(u32::from_le_bytes([n0, n1, n2, n3])),
+      ParWord::from_elem(u32::from_le_bytes([n4, n5, n6, n7])),
+      ParWord::from_elem(u32::from_le_bytes([n8, n9, n10, n11])),
+    ])
   }
 
-  const fn first_nonce_mut(&mut self) -> &mut u32 {
-    &mut self.nonces[0]
+  #[inline(always)]
+  const fn block_counter_mut(&mut self) -> &mut ParWord {
+    &mut self.0[12]
   }
 
-  const fn second_nonce_mut(&mut self) -> &mut u32 {
-    &mut self.nonces[1]
+  // https://datatracker.ietf.org/doc/html/rfc7539#section-2.4: ChaCha20 successively calls the
+  // ChaCha20 block function, with the same key and nonce, and with successively increasing block
+  // counter parameters.
+  //
+  // Not sure why an implementation would increase the nonces here.
+  #[inline(always)]
+  fn increment_counter(&mut self) {
+    // FIXME(STABLE): const { BLOCKS_LEN.try_into().unwrap() }
+    let blocks_len = ParWord::from_elem(u32::try_from(BLOCKS_LEN).unwrap_or_default());
+    *self.block_counter_mut() = self.block_counter_mut().wrapping_add(&blocks_len);
   }
 
-  const fn third_nonce_mut(&mut self) -> &mut u32 {
-    &mut self.nonces[2]
-  }
+  // https://datatracker.ietf.org/doc/html/rfc7539#section-2.1
+  #[inline(always)]
+  fn quarter_round(&mut self, a: usize, b: usize, c: usize, d: usize) -> Option<()> {
+    *self.0.get_mut(a)? = self.0.get(a)?.wrapping_add(self.0.get(b)?);
+    *self.0.get_mut(d)? = self.0.get(d)?.xor(self.0.get(a)?);
+    *self.0.get_mut(d)? = self.0.get(d)?.rotate_left(16);
 
-  const fn to_rows(&self) -> (Row, Row, Row, Row) {
-    let [c0, c1, c2, c3] = self.constants;
-    let [k0, k1, k2, k3, k4, k5, k6, k7] = self.keys;
-    let [n0, n1, n2] = self.nonces;
-    (
-      Row(c0, c1, c2, c3),
-      Row(k0, k1, k2, k3),
-      Row(k4, k5, k6, k7),
-      Row(self.block_counter, n0, n1, n2),
-    )
-  }
+    *self.0.get_mut(c)? = self.0.get(c)?.wrapping_add(self.0.get(d)?);
+    *self.0.get_mut(b)? = self.0.get(b)?.xor(self.0.get(c)?);
+    *self.0.get_mut(b)? = self.0.get(b)?.rotate_left(12);
 
-  const fn to_words(&self) -> [u32; WORDS] {
-    let [c0, c1, c2, c3] = self.constants;
-    let [k0, k1, k2, k3, k4, k5, k6, k7] = self.keys;
-    let [n0, n1, n2] = self.nonces;
-    [c0, c1, c2, c3, k0, k1, k2, k3, k4, k5, k6, k7, self.block_counter, n0, n1, n2]
+    *self.0.get_mut(a)? = self.0.get(a)?.wrapping_add(self.0.get(b)?);
+    *self.0.get_mut(d)? = self.0.get(d)?.xor(self.0.get(a)?);
+    *self.0.get_mut(d)? = self.0.get(d)?.rotate_left(8);
+
+    *self.0.get_mut(c)? = self.0.get(c)?.wrapping_add(self.0.get(d)?);
+    *self.0.get_mut(b)? = self.0.get(b)?.xor(self.0.get(c)?);
+    *self.0.get_mut(b)? = self.0.get(b)?.rotate_left(7);
+
+    Some(())
   }
 }
 
+/// Parallel Word
+///
+/// Represents the word position of one or more blocks
 #[cfg_attr(test, derive(Debug))]
-#[derive(Copy, Clone)]
-struct Row(u32, u32, u32, u32);
+#[derive(Clone, Copy, Default, Eq, Ord, PartialEq, PartialOrd)]
+struct ParWord([u32; BLOCKS_LEN]);
 
-impl Row {
-  const fn or(self, x: &Row) -> Row {
-    Row(self.0 | x.0, self.1 | x.1, self.2 | x.2, self.3 | x.3)
+impl ParWord {
+  #[inline(always)]
+  fn from_array(array: [u32; BLOCKS_LEN]) -> Self {
+    Self(array)
   }
 
-  const fn roll_left<const N: u8>(self) -> Row {
-    let lefted = self.shift_left::<N>();
-    let righted = self.shift_right(32u8.wrapping_sub(N));
-    lefted.or(&righted)
+  #[inline(always)]
+  fn from_elem(n: u32) -> Self {
+    let mut rslt = Self::default();
+    rslt.fill(n);
+    rslt
   }
 
-  const fn shift_left<const N: u8>(self) -> Row {
-    Row(self.0 << N, self.1 << N, self.2 << N, self.3 << N)
+  #[inline(always)]
+  fn fill(&mut self, n: u32) {
+    self.0 = [n; BLOCKS_LEN];
   }
 
-  const fn shuffle_left1(self) -> Row {
-    Row(self.1, self.2, self.3, self.0)
+  #[inline(always)]
+  fn rotate_left(&self, n: u32) -> Self {
+    let mut rslt = self.0;
+    for a in &mut rslt {
+      *a = a.rotate_left(n);
+    }
+    Self(rslt)
   }
 
-  const fn shuffle_left2(self) -> Row {
-    Row(self.2, self.3, self.0, self.1)
+  #[inline(always)]
+  fn wrapping_add(&self, other: &Self) -> Self {
+    let mut rslt = self.0;
+    for (a, b) in rslt.iter_mut().zip(other.0) {
+      *a = a.wrapping_add(b);
+    }
+    Self(rslt)
   }
 
-  const fn shuffle_left3(self) -> Row {
-    Row(self.3, self.0, self.1, self.2)
-  }
-
-  const fn shift_right(self, bit_distance: u8) -> Row {
-    Row(
-      self.0 >> bit_distance,
-      self.1 >> bit_distance,
-      self.2 >> bit_distance,
-      self.3 >> bit_distance,
-    )
-  }
-
-  const fn wrapping_add(self, x: &Row) -> Row {
-    Row(
-      self.0.wrapping_add(x.0),
-      self.1.wrapping_add(x.1),
-      self.2.wrapping_add(x.2),
-      self.3.wrapping_add(x.3),
-    )
-  }
-
-  const fn xor(self, x: &Row) -> Row {
-    Row(self.0 ^ x.0, self.1 ^ x.1, self.2 ^ x.2, self.3 ^ x.3)
+  #[inline(always)]
+  fn xor(&self, other: &Self) -> Self {
+    let mut rslt = self.0;
+    for (a, b) in rslt.iter_mut().zip(other.0) {
+      *a ^= b;
+    }
+    Self(rslt)
   }
 }
 
 // https://datatracker.ietf.org/doc/html/rfc7539#section-2.3
-const fn block_function<const ADD: bool>(block: &Block) -> Block {
-  let (mut a, mut b, mut c, mut d) = block.to_rows();
-
-  let mut idx = 0;
-  loop {
-    if idx >= ITERATIONS {
-      break;
-    }
-    round(&mut a, &mut b, &mut c, &mut d);
-    diagonalize(&mut b, &mut c, &mut d);
-    round(&mut a, &mut b, &mut c, &mut d);
-    undiagonalize(&mut b, &mut c, &mut d);
-    idx = idx.wrapping_add(1);
+#[inline(always)]
+fn block_function<const ADD: bool>(block: &ParBlock, output: &mut ParBlock) {
+  *output = *block;
+  for _ in 0..ITERATIONS {
+    let _ = output.quarter_round(0, 4, 8, 12);
+    let _ = output.quarter_round(1, 5, 9, 13);
+    let _ = output.quarter_round(2, 6, 10, 14);
+    let _ = output.quarter_round(3, 7, 11, 15);
+    let _ = output.quarter_round(0, 5, 10, 15);
+    let _ = output.quarter_round(1, 6, 11, 12);
+    let _ = output.quarter_round(2, 7, 8, 13);
+    let _ = output.quarter_round(3, 4, 9, 14);
   }
-
   if ADD {
-    let (e, f, g, h) = block.to_rows();
-    a = a.wrapping_add(&e);
-    b = b.wrapping_add(&f);
-    c = c.wrapping_add(&g);
-    d = d.wrapping_add(&h);
+    for (a, b) in output.0.iter_mut().zip(&block.0) {
+      *a = a.wrapping_add(b);
+    }
   }
-
-  Block::from_rows(a, b, c, d)
 }
 
-const fn diagonalize(b: &mut Row, c: &mut Row, d: &mut Row) {
-  *b = b.shuffle_left1();
-  *c = c.shuffle_left2();
-  *d = d.shuffle_left3();
-}
+#[cfg(all(feature = "_bench", test))]
+mod bench {
+  use crate::rng::{ChaCha20, Rng};
+  use core::hint::black_box;
 
-const fn undiagonalize(b: &mut Row, c: &mut Row, d: &mut Row) {
-  *b = b.shuffle_left3();
-  *c = c.shuffle_left2();
-  *d = d.shuffle_left1();
-}
-
-const fn round(a: &mut Row, b: &mut Row, c: &mut Row, d: &mut Row) {
-  *a = a.wrapping_add(b);
-  *d = a.xor(d);
-  *d = d.roll_left::<16>();
-
-  *c = c.wrapping_add(d);
-  *b = b.xor(c);
-  *b = b.roll_left::<12>();
-
-  *a = a.wrapping_add(b);
-  *d = a.xor(d);
-  *d = d.roll_left::<8>();
-
-  *c = c.wrapping_add(d);
-  *b = b.xor(c);
-  *b = b.roll_left::<7>();
+  #[bench]
+  fn cha_cha20(b: &mut test::Bencher) {
+    let mut rng = ChaCha20::from_key([7; 32]);
+    b.iter(|| {
+      black_box({
+        let mut array = [0; 1024 * 8];
+        rng.shuffle_slice(&mut array);
+      });
+    });
+  }
 }
 
 #[cfg(feature = "rand_core")]
 mod rand_core {
   use crate::rng::{ChaCha20, Rng};
+  use core::convert::Infallible;
 
-  impl rand_core::RngCore for ChaCha20 {
-    fn next_u32(&mut self) -> u32 {
-      u32::from_le_bytes(self.u8_4())
+  impl rand_core::TryCryptoRng for ChaCha20 {}
+
+  impl rand_core::TryRng for ChaCha20 {
+    type Error = Infallible;
+
+    #[inline(always)]
+    fn try_next_u32(&mut self) -> Result<u32, Self::Error> {
+      Ok(u32::from_le_bytes(self.u8_4()))
     }
 
-    fn next_u64(&mut self) -> u64 {
-      u64::from_le_bytes(self.u8_8())
+    #[inline(always)]
+    fn try_next_u64(&mut self) -> Result<u64, Self::Error> {
+      Ok(u64::from_le_bytes(self.u8_8()))
     }
 
-    fn fill_bytes(&mut self, dst: &mut [u8]) {
+    #[inline(always)]
+    fn try_fill_bytes(&mut self, dst: &mut [u8]) -> Result<(), Self::Error> {
       self.fill_slice(dst);
+      Ok(())
     }
   }
-
-  impl rand_core::CryptoRng for ChaCha20 {}
 }
 
 #[cfg(test)]
 mod tests {
   use crate::rng::{
     Rng, SeedableRng,
-    cha_cha20::{Block, ChaCha20, WORDS, block_function},
+    cha_cha20::{ChaCha20, ParBlock, WORDS_PER_BLOCK, block_function},
   };
 
   #[test]
@@ -375,10 +377,10 @@ mod tests {
       1,
     ])
     .unwrap();
-    for _ in 0..WORDS {
+    for _ in 0..WORDS_PER_BLOCK {
       let _ = this.u8_4();
     }
-    let mut results = [0u32; WORDS];
+    let mut results = [0u32; WORDS_PER_BLOCK];
     for elem in results.iter_mut() {
       *elem = u32::from_le_bytes(this.u8_4());
     }
@@ -395,7 +397,7 @@ mod tests {
     let mut this = ChaCha20::from_seed([0u8; 32]).unwrap();
     assert_eq!(
       {
-        let mut array = [0; WORDS];
+        let mut array = [0; WORDS_PER_BLOCK];
         for elem in &mut array {
           *elem = u32::from_le_bytes(this.u8_4());
         }
@@ -409,7 +411,7 @@ mod tests {
     );
     assert_eq!(
       {
-        let mut array = [0; WORDS];
+        let mut array = [0; WORDS_PER_BLOCK];
         for elem in &mut array {
           *elem = u32::from_le_bytes(this.u8_4());
         }
@@ -433,22 +435,25 @@ mod tests {
   // https://datatracker.ietf.org/doc/html/rfc7539#section-2.3.2
   #[test]
   fn rfc_2_3_2() {
-    let block = Block::from_words([
+    let block = ParBlock::from_words([
       0x61707865, 0x3320646e, 0x79622d32, 0x6b206574, 0x03020100, 0x07060504, 0x0b0a0908,
       0x0f0e0d0c, 0x13121110, 0x17161514, 0x1b1a1918, 0x1f1e1d1c, 0x00000001, 0x09000000,
       0x4a000000, 0x00000000,
     ]);
+    let mut output = block;
+    block_function::<false>(&block, &mut output);
     assert_eq!(
-      block_function::<false>(&block),
-      Block::from_words([
+      output,
+      ParBlock::from_words([
         0x837778ab, 0xe238d763, 0xa67ae21e, 0x5950bb2f, 0xc4f2d0c7, 0xfc62bb2f, 0x8fa018fc,
         0x3f5ec7b7, 0x335271c2, 0xf29489f3, 0xeabda8fc, 0x82e46ebd, 0xd19c12b4, 0xb04e16de,
         0x9e83d0cb, 0x4e3c50a2,
       ])
     );
+    block_function::<true>(&block, &mut output);
     assert_eq!(
-      block_function::<true>(&block),
-      Block::from_words([
+      output,
+      ParBlock::from_words([
         0xe4e7f110, 0x15593bd1, 0x1fdd0f50, 0xc47120a3, 0xc7f4d1c7, 0x0368c033, 0x9aaa2204,
         0x4e6cd4c3, 0x466482d2, 0x09aa9f07, 0x05d7c214, 0xa2028bd9, 0xd19c12b5, 0xb94e16de,
         0xe883d0cb, 0x4e3c50a2,
